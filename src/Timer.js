208e19cb8bf39efa7b7c0afad0d96269ead3481f
Guilherme Augusto <augusto.gui.2208@gmail.com>
2021-03-21 16:49:31 -0300

    Coutdown sendo criado

--- src/Timer.js
+++ src/Timer.js
@@ -1,13 +1,15 @@
 const Timer = {
-  time: 25 * 60, // 25 minutos x 60 segundos
+  time: 0.1 * 60, // 0.1 minutos x 60 segundos
   currentTime: 0,
 
   init() {
     Timer.currentTime = Timer.time // Timer pega time e atribui no currentTime
+    setInterval(Timer.countdown, 1000)
   },
 
   countdown() {
-
+    Timer.currentTime = Timer.currentTime - 1 // a cada um segundo, ele vai tirar um segundo
+    console.log(Timer.currentTime)
   }
 }
 
