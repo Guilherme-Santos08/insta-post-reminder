6ba3f221725ec26f99b00838b507b9841a13cb58
Guilherme Augusto <augusto.gui.2208@gmail.com>
2021-03-21 16:44:09 -0300

    Time e currentTime

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import { Timer } from './Timer.js';
 const App = {
    async start() {
       try {
+         Timer.init()
          // await Notifyer.init()
          // Notifyer.notify({
          //    title: "Hora do Post",
          //    body: "Crie algum conteúdo para ajudar a comunidade"
          // })
-
       } catch (err) {
          console.log(err.message)
       }
--- src/Timer.js
+++ src/Timer.js
@@ -1,6 +1,7 @@
 const Timer = {
+  time: 25 * 60, // 25 minutos x 60 segundos
+  currentTime: 0,
   
-
   init() {
   },
 
