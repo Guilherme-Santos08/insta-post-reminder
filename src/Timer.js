583904e5c1aea31c93543ea5449b1afca11dd525
Guilherme Augusto <augusto.gui.2208@gmail.com>
2021-03-21 16:57:28 -0300

    Countdown feito

--- src/Timer.js
+++ src/Timer.js
@@ -1,15 +1,21 @@
 const Timer = {
   time: 0.1 * 60, // 0.1 minutos x 60 segundos
   currentTime: 0,
-
+  interval: null, // Criando a variavel para o countdown não ficar contando para sempre
   init() {
     Timer.currentTime = Timer.time // Timer pega time e atribui no currentTime
-    setInterval(Timer.countdown, 1000)
+    Timer.interval = setInterval(Timer.countdown, 1000)
   },
 
   countdown() {
     Timer.currentTime = Timer.currentTime - 1 // a cada um segundo, ele vai tirar um segundo
     console.log(Timer.currentTime)
+
+    if(Timer.currentTime === 0) {
+      clearInterval(Timer.interval) // Vai cancelar o countdown
+      return
+    }
+    
   }
 }
 
