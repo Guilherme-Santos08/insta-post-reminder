25d46ec91303e914da965509405c99cf8e7b5bc8
Guilherme Augusto <augusto.gui.2208@gmail.com>
2021-03-21 16:46:00 -0300

    Timer.currentTime

--- src/Timer.js
+++ src/Timer.js
@@ -1,8 +1,9 @@
 const Timer = {
   time: 25 * 60, // 25 minutos x 60 segundos
   currentTime: 0,
-  
+
   init() {
+    Timer.currentTime = Timer.time // Timer pega time e atribui no currentTime
   },
 
   countdown() {
