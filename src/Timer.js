fb3a95052e47453e30eb287d72d4454ba664adad
Guilherme Augusto <augusto.gui.2208@gmail.com>
2021-03-21 17:45:07 -0300

    Fazendo aparecer na tela

--- src/Timer.js
+++ src/Timer.js
@@ -4,6 +4,10 @@ const Timer = {
   time: 0.1 * 60, // 0.1 minutos x 60 segundos
   currentTime: 0,
   interval: null, // Criando a variavel para o countdown não ficar contando para sempre
+
+  timeToMinutes: time => Math.floor(time / 60),
+  timeToSeconds: time => time % 60, // Oque sobrar da divisão ele vai colocar aqui
+
   init() {
     Timer.currentTime = Timer.time // Timer pega time e atribui no currentTime
     Timer.interval = setInterval(Timer.countdown, 1000)
@@ -11,7 +15,13 @@ const Timer = {
 
   countdown() {
     Timer.currentTime = Timer.currentTime - 1 // a cada um segundo, ele vai tirar um segundo
-    console.log(Timer.currentTime)
+
+    console.log(Timer.timeToMinutes(Timer.currentTime), ":", Timer.timeToSeconds(Timer.currentTime))
+
+    // View.render({
+    //   minutes: "25",
+    //   seconds: "19"
+    // })
 
     if(Timer.currentTime === 0) {
       clearInterval(Timer.interval) // Vai cancelar o countdown
