3fd1aa686b09ef9bbcdb71de0eb6f3b394972fff
Guilherme Augusto <augusto.gui.2208@gmail.com>
2021-03-22 22:56:38 -0300

    Exportando o Emitter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Notifyer } from './Notifyer.js';
 import { Timer } from './Timer.js';
+import { Emitter } from './Emitter.js'
 
 const App = {
    async start() {
       try {
-         Timer.init()
+         Timer.init(0.1 * 60)
+         Emitter.init()
          // await Notifyer.init()
          // Notifyer.notify({
          //    title: "Hora do Post",
---
+++ src/Emitter.js
@@ -0,0 +1,5 @@
+const Emitter = {
+
+}
+
+export { Emitter }
