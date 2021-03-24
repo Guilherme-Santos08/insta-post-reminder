0615955a482e868e1d45acfa8b4a6502b4b950ae
Guilherme Augusto <augusto.gui.2208@gmail.com>
2021-03-24 12:07:53 -0300

    Gerando frases aleatorias

--- src/App.js
+++ src/App.js
@@ -2,20 +2,35 @@ import { Notifyer } from './Notifyer.js';
 import { Timer } from './Timer.js';
 import { Emitter } from './Emitter.js'
 
-const notify = Notifyer.notify({
+
+const messages = [
+  "Postar foto do meu stup",
+  "Postar uma frase motivacional" ,
+  "Ensinar algo que aprendi hoje ou ontem",
+  "Crie algum conteúdo para ajudar a comunidade"
+];
+
+const notify = () => {
+
+   const randonIndex = Math.floor(Math.random() * messages.length)
+
+   const notification = Notifyer.notify({
    title: "Hora do Post",
-   body: "Crie algum conteúdo para ajudar a comunidade"
+   body: messages[randonIndex]
 })
+
+   notification()
+}
 
 const App = {
    async start() {
       try {
-         await Notifyer.ini
+         await Notifyer.init()
 
          Emitter.on('countdown-start', notify)
          Emitter.on('countdown-end', Timer.init)
          
-         Timer.init()
+         Timer.init(0.1 * 60)
       } catch (err) {
          console.log(err.message)
       }
